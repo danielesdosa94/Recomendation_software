0aa3b5a2ae65608b9ceb261db53e1d39f4902df2
danielesdosa94 <danielesdosa@gmail.com>
2025-04-08 16:37:16 -0500

    Describe los cambios: eliminación de readme.txt y actualización en welcome.py

---
+++ welcome.py
@@ -0,0 +1,29 @@
+def print_waveform():
+    # Dibujo de la forma de onda
+    print("         *         *         *         ")
+    print("        * *       * *       * *        ")
+    print("       *   *     *   *     *   *       ")
+    print("      *     *   *     *   *     *      ")
+    print("     *       * *       * *       *     ")
+    print("    *         *         *         *    ")
+    print("     *       * *       * *       *     ")
+    print("      *     *   *     *   *     *      ")
+    print("       *   *     *   *     *   *       ")
+    print("        * *       * *       * *        ")
+    print("         *         *         *         ")
+
+    # Líneas separadoras (pueden simbolizar un “head” o división)
+    print("***************************************")
+    print("***************************************")
+
+    # Marco inferior con mensaje de bienvenida (ancho fijo de 39 caracteres)
+    print("_______________________________________")
+    print("*                                     *")
+    print("*                                     *")
+    print("* Bienvenido a la Recomendación        *")
+    print("* de Plugins de Audio!                 *")
+    print("*                                     *")
+    print("*_____________________________________*")
+
+
+
